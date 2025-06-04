3e657b05a1ee4a21ca6bb07f1b89f6ca92582e9d
yash-wizards <yashmalawant99@gmail.com>
2025-06-04 19:08:27 +0530

    update app configuration - Updated App.tsx with transparent status bar, Added new dependencies in package.json, Updated iOS/Android build configurations

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { View, StatusBar } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar translucent backgroundColor="transparent" />
+      <AppNavigator />
+    </View>
+  );
 };
 
 export default App;
